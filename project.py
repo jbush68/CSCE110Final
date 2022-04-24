9dad21a90a0e71a3ac28ab23e47f860b901b78e3
Joshua <joshua.bush20@gmail.com>
2022-04-24 01:45:56 -0500

    Implemented creation of classrooms and reading the .csv file, kinda messy, can probably afford to clean it up later.

--- project.py
+++ project.py
@@ -12,6 +12,21 @@ class StudentDict(TypedDict):
     project: float
 
 
+def cast_student_dict(student) -> StudentDict or None:
+    try:
+        uin = str(student[0])
+        labs = [float(_) for _ in student[1:6]]
+        quizzes = [float(_) for _ in student[7:12]]
+        readings = [float(_) for _ in student[13:18]]
+        exams = [float(_) for _ in student[19:21]]
+        project = float(student[23])
+    except ValueError:
+        print(f'Invalid student in file')
+        return None
+
+    return StudentDict(uin=uin, labs=labs, quizzes=quizzes, readings=readings, exams=exams, project=project)
+
+
 class Student:
     def __init__(self, student_data: StudentDict):
         self.uin = student_data["uin"]
@@ -20,6 +35,28 @@ class Student:
         self.readings = student_data["readings"]
         self.exams = student_data["exams"]
         self.project = student_data["project"]
+
+
+class ClassSet:
+    def __init__(self):
+        self.students = list[Student]
+        self.num_students = len(self.students)
+
+    def populate_class(self):
+        file_path = str(input('Enter file path: '))
+        students = []
+        with open(file_path, newline='') as class_file:
+            student_list = csv.reader(class_file, delimiter=',')
+            for index, row in enumerate(student_list):
+                if index == 0:
+                    continue
+                data = cast_student_dict(row)
+                if not data:
+                    continue
+                else:
+                    students.append(Student(data))
+
+        self.students = students
 
 
 def menu() -> int:
